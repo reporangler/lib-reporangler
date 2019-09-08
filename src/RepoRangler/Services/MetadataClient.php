eb36e02dd2244b18f45d18c3db6950f2018cc0a6
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-09-09 00:25:58 +0200

    add the functionality to add a package to the metadata service

--- src/RepoRangler/Services/MetadataClient.php
+++ src/RepoRangler/Services/MetadataClient.php
@@ -23,15 +23,33 @@ class MetadataClient
         $this->httpClient = $httpClient;
     }
 
-    public function getPackages($token): array
+    public function getPackages(string $token, string $repositoryType): array
     {
         $response = $this->httpClient->get($this->baseUrl.'/packages', [
             'headers' => [
                 'Authorization' => 'Bearer ' . $token,
                 'Accept' => 'application/json',
+                'reporangler-repository-type' => $repositoryType,
             ],
         ]);
 
         return json_decode((string)$response->getBody(), true);
+    }
+
+    public function addPackage(string $token, string $repositoryType, string $packageGroup, string $packageName, string $packageVersion, array $definition): array
+    {
+        $response = $this->httpClient->post($this->baseUrl.'/packages', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Accept' => 'application/json',
+            ],
+            RequestOptions::JSON => [
+                'name' => $packageName,
+                'version' => $packageVersion,
+                'definition' => $definition,
+                'package_group' => $packageGroup,
+                'repository_type' => $repositoryType,
+            ]
+        ]);
     }
 }
