c0e72cd5497e58575c04c4d3834638871002e889
Chris Thomas <christopher.thomas@plista.com>
2019-10-04 13:34:29 +0200

    some entities should have patterns which control what values they're allowed to be created with. Although this is not yet implemented in the other fields yet

--- src/RepoRangler/Entity/PackageGroup.php
+++ src/RepoRangler/Entity/PackageGroup.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class PackageGroup extends Model
 {
     const PUBLIC_GROUP = 'public';
+    const PATTERN = '[a-z][a-z0-9\-\.]+';
 
     protected $fillable = ['id', 'name'];
 }
