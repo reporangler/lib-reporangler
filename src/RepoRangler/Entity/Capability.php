60d55585afaf9cb3a541011a7b0d84edf2ce673a
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-10-07 01:01:47 +0200

    Add the ability to be an administrator of an entire repository type

--- src/RepoRangler/Entity/Capability.php
+++ src/RepoRangler/Entity/Capability.php
@@ -5,13 +5,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Capability extends Model
 {
-    const IS_PUBLIC_USER = 'IS_PUBLIC_USER';
-    const IS_ADMIN_USER = 'IS_ADMIN_USER';
-    const IS_REST_USER = 'IS_REST_USER';
-    const IS_REPO_USER = 'IS_REPO_USER';
+    const IS_PUBLIC_USER        = 'IS_PUBLIC_USER';
+    const IS_ADMIN_USER         = 'IS_ADMIN_USER';
+    const IS_REST_USER          = 'IS_REST_USER';
+    const IS_REPO_USER          = 'IS_REPO_USER';
 
-    const REPOSITORY_ACCESS = 'REPOSITORY_ACCESS';
+    const REPOSITORY_ADMIN      = 'REPOSITORY_ADMIN';
+    const REPOSITORY_ACCESS     = 'REPOSITORY_ACCESS';
 
-    const PACKAGE_GROUP_ADMIN = 'PACKAGE_GROUP_ADMIN';
-    const PACKAGE_GROUP_ACCESS = 'PACKAGE_GROUP_ACCESS';
+    const PACKAGE_GROUP_ADMIN   = 'PACKAGE_GROUP_ADMIN';
+    const PACKAGE_GROUP_ACCESS  = 'PACKAGE_GROUP_ACCESS';
 }
