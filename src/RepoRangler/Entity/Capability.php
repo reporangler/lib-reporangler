ea768c941fe424741e83cc944079aa39b6652046
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-10-24 19:24:31 +0200

    The XXX_ADMIN capabilities were merged into the other capabilities of similar name, now we just add an extra constraint field 'admin' = true, to say whether the capability has admin privileges or not

--- src/RepoRangler/Entity/Capability.php
+++ src/RepoRangler/Entity/Capability.php
@@ -10,9 +10,6 @@ class Capability extends Model
     const IS_REST_USER          = 'IS_REST_USER';
     const IS_REPO_USER          = 'IS_REPO_USER';
 
-    const REPOSITORY_ADMIN      = 'REPOSITORY_ADMIN';
     const REPOSITORY_ACCESS     = 'REPOSITORY_ACCESS';
-
-    const PACKAGE_GROUP_ADMIN   = 'PACKAGE_GROUP_ADMIN';
     const PACKAGE_GROUP_ACCESS  = 'PACKAGE_GROUP_ACCESS';
 }
