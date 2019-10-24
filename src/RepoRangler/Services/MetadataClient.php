8e0e8056df09c12f1417715d65b16b9331af8cd5
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-10-24 20:55:47 +0200

    add more functionality for creating/getting repository and package groups from the metadata service

--- src/RepoRangler/Services/MetadataClient.php
+++ src/RepoRangler/Services/MetadataClient.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Collection;
 use Psr\Http\Message\ResponseInterface;
 use RepoRangler\Entity\PackageGroup;
 use RepoRangler\Entity\Repository;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 class MetadataClient
 {
@@ -33,6 +34,37 @@ class MetadataClient
         ];
     }
 
+    private function getRequestData(?array $fields = null): array
+    {
+        $data = ['headers' => $this->getHeaders()];
+
+        if(!empty($fields)){
+            $data[RequestOptions::JSON] = $fields;
+        }
+
+        return $data;
+    }
+
+    private function decode(ResponseInterface $response)
+    {
+        return json_decode((string)$response->getBody(), true);
+    }
+
+    private function decodeList(ResponseInterface $response, string $type)
+    {
+        $response = $this->decode($response);
+
+        if(!array_key_exists('data', $response)){
+            $response['data'] = collect($response['data'])->map(function($value) use ($type) {
+                return new $type($value);
+            });
+
+            return $response;
+        }
+
+        throw new UnprocessableEntityHttpException('This endpoint did not return a list as expected');
+    }
+
 	public function __construct(string $baseUrl, Client $httpClient, string $token)
 	{
 		$this->baseUrl = $baseUrl;
@@ -42,70 +74,80 @@ class MetadataClient
 
 	public function getPackages(string $repositoryType): array
 	{
-		$response = $this->httpClient->get("$this->baseUrl/packages/$repositoryType", [
-			'headers' => $this->getHeaders(),
-		]);
+		$response = $this->httpClient->get("$this->baseUrl/packages/$repositoryType", $this->getRequestData());
 
 		return json_decode((string)$response->getBody(), true);
 	}
 
 	public function addPackage(string $repositoryType, string $packageGroup, string $packageName, string $packageVersion, array $definition): array
 	{
-		$response = $this->httpClient->post("$this->baseUrl/packages/$repositoryType", [
-            'headers' => $this->getHeaders(),
-			RequestOptions::JSON => [
-				'name' => $packageName,
-				'version' => $packageVersion,
-				'definition' => $definition,
-				'package_group' => $packageGroup,
-			]
-		]);
+		$response = $this->httpClient->post("$this->baseUrl/packages/$repositoryType", $this->getRequestData([
+            'name' => $packageName,
+            'version' => $packageVersion,
+            'definition' => $definition,
+            'package_group' => $packageGroup,
+        ]));
 
 		return json_decode((string)$response->getBody(), true);
 	}
 
+	public function createPackageGroup(string $name): PackageGroup
+    {
+        $response = $this->httpClient->post("$this->baseUrl/package-group", $this->getRequestData([
+            'name' => $name;
+        ]));
+
+        return new PackageGroup($this->decode($response));
+    }
+
+    public function getPackageGroupList(): Collection
+    {
+        $response = $this->httpClient->get("$this->baseUrl/package-group", $this->getRequestData());
+
+        return $this->decodeList($response, PackageGroup::class);
+    }
+
 	public function getPackageGroupById(int $id): PackageGroup
 	{
-		$response = $this->httpClient->get("$this->baseUrl/package-group/$id", [
-            'headers' => $this->getHeaders(),
-		]);
+		$response = $this->httpClient->get("$this->baseUrl/package-group/$id", $this->getRequestData());
 
-		return new PackageGroup(json_decode((string)$response->getBody(), true));
+		return new PackageGroup($this->decode($response));
 	}
 
     public function getPackageGroupByName(string $name): PackageGroup
     {
-        $response = $this->httpClient->get("$this->baseUrl/package-group/$name", [
-            'headers' => $this->getHeaders(),
-        ]);
+        $response = $this->httpClient->get("$this->baseUrl/package-group/$name", $this->getRequestData());
 
-        return new PackageGroup(json_decode((string)$response->getBody(), true));
+        return new PackageGroup($this->decode($response));
+    }
+
+    public function createRepository(string $name): Repository
+    {
+        $response = $this->httpClient->post("$this->baseUrl/repository", $this->getRequestData([
+            'name' => $name;
+        ]));
+
+        return new Repository($this->decode($response));
     }
 
     public function getRepositoryList(): Collection
     {
-        $response = $this->httpClient->get("$this->baseUrl/repository", [
-            'headers' => $this->getHeaders(),
-        ]);
+        $response = $this->httpClient->get("$this->baseUrl/repository", $this->getRequestData());
 
-        return collect(json_decode((string)$response->getBody(), true));
+        return $this->decodeList($response, Response::class);
     }
 
 	public function getRepositoryById(int $id): Repository
     {
-        $response = $this->httpClient->get("$this->baseUrl/repository/$id", [
-            'headers' => $this->getHeaders(),
-        ]);
+        $response = $this->httpClient->get("$this->baseUrl/repository/$id", $this->getRequestData());
 
-        return new Repository(json_decode((string)$response->getBody(), true));
+        return new Repository($this->decode($response));
     }
 
     public function getRepositoryByName(string $name): Repository
     {
-        $response = $this->httpClient->get("$this->baseUrl/repository/$name", [
-            'headers' => $this->getHeaders(),
-        ]);
+        $response = $this->httpClient->get("$this->baseUrl/repository/$name", $this->getRequestData());
 
-        return new Repository(json_decode((string)$response->getBody(), true));
+        return new Repository($this->decode($response));
     }
 }
